94bb5644121790dfdb2969e453c9d5cfbf308eb7
Parker Moore <parkrmoore@gmail.com>
2013-10-05 22:14:44 -0400

    Update Date plugin to work with Jekyll v1.x.x

--- plugins/date.rb
+++ plugins/date.rb
@@ -51,24 +51,29 @@ module Jekyll
   class Post
     include Octopress::Date
 
+    def date_formatted
+      date_format = self.site.config['date_format']
+      format_date(self.date, date_format)
+    end
+
+    def updated_formatted
+      self.data.has_key?('updated') ? format_date(self.data['updated'], date_format) : nil
+    end
+
     # Convert this post into a Hash for use in Liquid templates.
     #
     # Returns <Hash>
-    def to_liquid
-      date_format = self.site.config['date_format']
-      self.data.deep_merge({
-        "title"             => self.data['title'] || self.slug.split('-').select {|w| w.capitalize! || w }.join(' '),
-        "url"               => self.url,
-        "date"              => self.date,
-        # Monkey patch
-        "date_formatted"    => format_date(self.date, date_format),
-        "updated_formatted" => self.data.has_key?('updated') ? format_date(self.data['updated'], date_format) : nil,
-        "id"                => self.id,
-        "categories"        => self.categories,
-        "next"              => self.next,
-        "previous"          => self.previous,
-        "tags"              => self.tags,
-        "content"           => self.content })
+    def to_liquid(attrs = ATTRIBUTES_FOR_LIQUID)
+      super(attrs + %w[
+        date_formatted
+        updated_formatted
+        id
+        categories
+        next
+        previous
+        tags
+        content
+      ])
     end
   end
 
