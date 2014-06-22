25e5ee5e4200117b4ccc1a93942577bf7ea8a4dd
Parker Moore <parkrmoore@gmail.com>
2014-06-22 01:26:50 -0400

    Update to what Brandon has in octopress#1590.
    https://github.com/imathis/octopress/pull/1590

--- plugins/octopress_filters.rb
+++ plugins/octopress_filters.rb
@@ -1,40 +1,8 @@
 # Custom filters for Octopress
 require 'jekyll-page-hooks'
-require './plugins/backtick_code_block'
+require 'octopress-codefence'
+require 'octopress-gist'
 require './plugins/raw'
-require 'rubypants'
-
-module OctopressFilters
-  include BacktickCodeBlock
-  include TemplateWrapper
-  def pre_filter(input)
-    input = render_code_block(input)
-    input.gsub /(<figure.+?>.+?<\/figure>)/m do
-      safe_wrap($1)
-    end
-  end
-  def post_filter(input)
-    input = unwrap(input)
-    RubyPants.new(input).to_html
-  end
-end
-
-module Jekyll
-  class ContentFilters < PageHooks
-    include OctopressFilters
-    def pre_render(post)
-      if post.ext.match('html|textile|markdown|md|haml|slim|xml')
-        post.content = pre_filter(post.content)
-      end
-    end
-    def post_render(post)
-      if post.ext.match('html|textile|markdown|md|haml|slim|xml')
-        post.content = post_filter(post.content)
-      end
-    end
-  end
-end
-
 
 module OctopressLiquidFilters
   include Octopress::Date
